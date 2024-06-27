e3a3bfd7d0bcdc83d7c4e87599ff397d3a13ec78
fine <92whdtjs@gmail.com>
2024-06-27 15:40:42 +0900

    #5 - Entity - User
    User Entity의 경우에는 현재 엔티티 설계만 해두고 로그인 기능을 붙일 때 각 엔티티와의 관계를 고려한다.
    - id
    - email : nullable, unique : 프로퍼티 값 변경은 가능하나 별도 중복확인 절차를 거친다.
    - password: nullabe, unique : 프로퍼티 변경은 가능하나 별도의 암호화 로직을 거친다.
    -  nickname: 프로퍼티 변경은 가능하나 별도의 중복확인 절차를 거친다.
    - createdAt, updateAt은 이후 Config를 통해 JPA가 자동으로 찍어주도록 한다.

---
+++ WeWriteB/src/main/java/com/onemorethink/wewriteb/domain/User.java
@@ -0,0 +1,37 @@
+package com.onemorethink.wewriteb.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+import java.time.LocalDateTime;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "email"),
+        @Index(columnList = "nickname"),
+        @Index(columnList = "createdAt")
+})
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true)
+    private LocalDateTime updateAt;
+}
