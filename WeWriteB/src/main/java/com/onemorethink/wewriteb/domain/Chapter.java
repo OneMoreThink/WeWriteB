6b4e4ef9d3997ebb42d48b127c479317c97196df
fine <92whdtjs@gmail.com>
2024-06-27 16:51:16 +0900

    #5 - Entity - Chapter
    - id : Long
    - title: String
    - content: String
    
    * Novel Entity에 @OneToMany Chapter
    * Chapter Entity에 ManyToOne Novel
    Cascade.All & orphanRemoval

--- WeWriteB/src/main/java/com/onemorethink/wewriteb/domain/Chapter.java
+++ WeWriteB/src/main/java/com/onemorethink/wewriteb/domain/Chapter.java
@@ -5,27 +5,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @ToString
-@Table(indexes = {
-        @Index(columnList = "title"),
-        @Index(columnList = "createdAt")
-})
 @Entity
-public class Novel {
+public class Chapter {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    /* @ManyToOne
-     * JoinColum 외래키로 사용될 열 이름을 지정
-     * @JoinColumn(name = "author_id", nullable = false)
-     * private User author;
-     * 로그인 기능이 붙었을 때 해당 필드 활성화
-     */
 
     @Column(nullable = false)
     private String title;
@@ -33,14 +21,13 @@ public class Novel {
     @Column(nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "novel", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Chapter> chapters;
+    @ManyToOne
+    @JoinColumn(name = "novel_id", nullable = false)
+    private Novel novel;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @Column(nullable = true)
     private LocalDateTime updatedAt;
-
-
 }
--- WeWriteB/src/main/java/com/onemorethink/wewriteb/domain/User.java
+++ WeWriteB/src/main/java/com/onemorethink/wewriteb/domain/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
 import java.time.LocalDateTime;
-import java.util.List;
 
 
 @Getter
